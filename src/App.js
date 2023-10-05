5e346ad62cb7566b2d0638b0f00b377c303c1cb8
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-10-05 09:19:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header.js';
+import About from './components/about.js';
+import Skill from './components/skill.js';
+import Projects from './components/project.js';
+import Contact from './components/contact.js';
+import './style/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Header />
+      <About/>
+      <Skill/>
+      <Projects/>
+      <Contact/>
+      {/* Add other components (About, Projects, Contact) here */}
     </div>
   );
 }

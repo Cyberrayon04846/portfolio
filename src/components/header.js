c71cfb54e4677b26bec8012b9095fa7719e16730
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-10-05 09:20:38 +0530

    Create header.js

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../style/header.scss';
+
+const Header = () => {
+ 
+  return (
+    <header className="header">
+      <h2>{'<Preet Chahal/>'}</h2>
+    </header>
+  );
+};
+
+export default Header;

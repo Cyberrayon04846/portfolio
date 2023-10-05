d75b2a69d21e5ef2eca58051254629254c7a6d5f
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-10-05 11:32:05 +0530

    Update about.js

--- src/components/about.js
+++ src/components/about.js
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from 'react';
-import '../style/about.scss';
-import developerImage from '../assests/devloperImage.png'; // Import your developer image
-import resumePDF from '../assests/resume.pdf'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import React, { useEffect, useState } from "react";
+import "../style/about.scss";
+import developerImage from "../assests/devloperImage.png"; // Import your developer image
+import resumePDF from "../assests/resume.pdf";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faDownload } from "@fortawesome/free-solid-svg-icons";
 
 const About = () => {
   const [animate, setAnimate] = useState(false);
@@ -13,24 +13,22 @@ const About = () => {
   }, []);
 
   return (
-    <div className={`about ${animate ? 'about-animate' : ''}`}>
+    <div className={`about ${animate ? "about-animate" : ""}`}>
       <div className="about-content">
         <div className="about-text">
-          <div className="about-heading">
-            Hi, I'm Myself preet chahal
-          </div>
+          <div className="about-heading">Hi, I'm Myself preet chahal</div>
           <div className="about-description">
-            A passionate mern stack developer from India
-            A passionate mern stack developer from India
-            A passionate mern stack developer from India
-            A passionate mern stack developer from India
-            A passionate mern stack developer from India
-            A passionate mern stack developer from India
+            Passionate in programming and cyber security with 2+year of
+            experience in web development .Knowledgeable in
+            HTML,CSS,JavaScript,Mern ,PHP,C++.Currently learning blockchain
             {/* ... (your description) ... */}
           </div>
           <div className="resume-button">
             <a href={resumePDF} download="resume.pdf">
-            <FontAwesomeIcon icon={faDownload} style={{ marginRight: '8px' }} />
+              <FontAwesomeIcon
+                icon={faDownload}
+                style={{ marginRight: "8px" }}
+              />
               Download Resume
             </a>
           </div>

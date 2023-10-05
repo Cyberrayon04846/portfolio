7d2d84040fbecfc9e168e064f21d0f0bf3c79ce9
Cyberrayon04846 <90168879+Cyberrayon04846@users.noreply.github.com>
2023-10-05 09:20:46 +0530

    Create skill.js

---
+++ src/components/skill.js
@@ -0,0 +1,63 @@
+import React, { useEffect, useState } from 'react';
+import '../style/skill.scss';
+import skillImage from '../assests/skillimage.png';
+const skillsData = [
+    { name: 'JavaScript', experience: 90 },
+    { name: 'React', experience: 85 },
+    { name: 'Node.js', experience: 80 },
+    { name: 'HTML/CSS', experience: 95 },
+    // Add more skills and experience levels here
+  ];
+  
+  const Skill = () => {
+    const [animate, setAnimate] = useState(false);
+  
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        (entries) => {
+          entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+              setAnimate(true);
+            }
+          });
+        },
+        { threshold: 0.5 }
+      );
+  
+      observer.observe(document.querySelector('.skills-container'));
+  
+      // Clean up the observer on component unmount
+      return () => {
+        observer.disconnect();
+      };
+    }, []);
+  
+    return (
+      <div className={`skills-container ${animate ? 'animate' : ''}`}>
+        <div className="skill-image">
+        <img src={skillImage} alt="Skill" />
+      </div>
+      <div className="skills-content">
+        <h2>Skills</h2>
+        
+        <div className="skills-list">
+        {skillsData.map((skill, index) => (
+          <div className="skill" key={index}>
+            <div className="skill-content">
+              <div className="skill-name">{skill.name}</div>
+              <div className="percentage">{skill.experience}%</div>
+            </div>
+            <div className="progress-bar">
+              <div
+                className="progress"
+                style={{ width: `${skill.experience}%` }}
+              ></div>
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
+      </div>
+    );
+  };
+export default Skill;

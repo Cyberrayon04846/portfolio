eb14bdfe12177772f161dbf3a245982c0a41f49c
Cyberrayon04846 <cyberrayon@gmail.com>
2024-01-10 10:20:56 +0530

    Update project.js

--- src/components/project.js
+++ src/components/project.js
@@ -3,29 +3,33 @@ import ProjectCard from "./ProjectCard";
 import "../style/projects.scss";
 import chatImage from "../assests/chatImage.png";
 import portfolio from "../assests/portfolio.png";
+
+// Data for the projects
 const projectsData = [
   {
     title: "Chat app",
-    description: "A chat apps where you can discus about diffrent topic in chat rooms",
+    description: "A chat app where you can discuss about different topics in chat rooms",
     githubLink: "https://github.com/Cyberrayon04846/mychat",
     image: chatImage,
   },
   {
     title: "Personal Portfolio",
-    description: "MY presonl created using react js",
+    description: "My personal portfolio created using React.js",
     githubLink: "https://github.com/Cyberrayon04846/portfolio",
     image: portfolio,
   },
- 
- 
 ];
 
+/**
+ * Component that renders the projects section.
+ */
 const Projects = () => {
   return (
     <div className="projects-container">
       <div className="projects-content">
         <h2>My Work</h2>
         <div className="project-cards">
+          {/* Render project cards */}
           {projectsData.map((project, index) => (
             <ProjectCard
               key={index}
